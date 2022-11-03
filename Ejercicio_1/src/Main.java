a32738d6d8387044b69688f30aa2d8df86cde31b
Crowlyto <leandro.n.nunez@gmail.com>
2022-11-03 11:42:37 -0300

    Ejercicio 1

---
+++ Ejercicio_1/src/Main.java
@@ -0,0 +1,32 @@
+public class Main {
+    public static void main(String[] args) {
+        //Tipos de Datos
+
+        int numero=10;
+        long numero1=8;
+        short numero3=4;
+        byte numero4=1;
+        float numero5=2.5f;
+        double numero6=2.5;
+        String texto="Hola";
+        boolean booleano=true;
+        boolean booleano1=false;
+        Integer num=null;
+        Long num1=null;
+
+        //Impresion por pantalla de datos
+
+        System.out.println("int: "+numero+"");
+        System.out.println("long: "+numero1+"");
+        System.out.println("short: "+numero3+"");
+        System.out.println("byte: "+numero4+"");
+        System.out.println("float: "+numero5+"");
+        System.out.println("double: "+numero6+"");
+        System.out.println("String: "+texto+"");
+        System.out.println("boolean: "+booleano+"");
+        System.out.println("boolean: "+booleano1+"");
+        System.out.println("Integer: "+num+"");
+        System.out.println("Long: "+num1+"");
+
+    }
+}
